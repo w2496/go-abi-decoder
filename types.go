4015aeb25aee2da6e18a02f151695dff85312703
w2496 <139476228+w2496@users.noreply.github.com>
2023-07-19 20:14:02 +0200

    Update types.go

--- types.go
+++ types.go
@@ -1,15 +1,12 @@
 package decoder
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"regexp"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type Params map[string]interface{}
@@ -37,6 +34,21 @@ func (m *Params) MarshalJSON() ([]byte, error) {
 	result += "}"
 
 	return []byte(result), nil
+}
+
+type ScannedLogs []DecodedLog
+
+func (l *ScannedLogs) ToJSONBytes() []byte {
+	res, err := json.Marshal(l)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
+
+func (l *ScannedLogs) ToJSON() string {
+	return string(l.ToJSONBytes())
 }
 
 // DecodedLog is a struct for holding decoded Ethereum logs.
@@ -136,103 +148,4 @@ func (data *DecodedMethod) GetSigHash() string {
 	}
 
 	return "0x" + data.SigHash
-}
-
-// AbiStorage is a struct for holding Ethereum ABIs.
-type AbiStorage struct {
-	Address  common.Address `json:"address"`            // Address of the contract the ABI belongs to.
-	Abi      abi.ABI        `json:"abi"`                // The ABI of the contract.
-	Bytecode *string        `json:"bytecode,omitempty"` // Bytecode of the contract the ABI belongs to.
-	IsToken  bool           `json:"isToken"`            // Current ABI is a Token
-	Verified bool           `json:"verified"`           // Whether the ABI has been verified.
-	IsERC721 *bool          `json:"isERC721,omitempty"` // contract is NFT Token
-	Name     *string        `json:"name,omitempty"`     // Name of the contract
-	Pragma   *string        `json:"pragma,omitempty"`   // Pragma Solidity Version of contract
-	Source   *string        `json:"source,omitempty"`   // Solidity source code of contract
-	client   *ethclient.Client
-}
-
-// ToJSONBytes returns the JSON-encoded byte array of the AbiStorage object.
-func (data *AbiStorage) ToJSONBytes() []byte {
-	b, _ := json.Marshal(data)
-	return b
-}
-
-// ToJSON returns the JSON-encoded string of the AbiStorage object.
-func (data *AbiStorage) ToJSON() string {
-	return string(data.ToJSONBytes())
-}
-
-// ToJSON returns the JSON-encoded string of the AbiStorage object.
-func (data *AbiStorage) GetBytecode(address common.Address) *string {
-	if data.Bytecode == nil && data.client != nil {
-		code, err := data.client.CodeAt(context.Background(), address, nil)
-		if err == nil && code != nil {
-			_bytecode := strings.Join([]string{"0x", common.Bytes2Hex(code)}, "")
-			*data.Bytecode = _bytecode
-		}
-	}
-
-	return data.Bytecode
-}
-
-// returns a single decoder instance of given AbiStorage object
-func (data *AbiStorage) GetDecoder() AbiDecoder {
-	contractAddress := data.Address.Hex()
-	return AbiDecoder{
-		ContractAddress: &contractAddress,
-		Abi:             &data.Abi,
-		IsVerified:      data.Verified,
-		client:          data.client,
-	}
-}
-
-// gets all signature hashes of given ABI
-func (data *AbiStorage) GetSigHashes() []string {
-	result := make([]string, 0)
-
-	for _, method := range data.Abi.Methods {
-		sigHash := ToSHA3(method.Sig)
-		result = append(result, sigHash[:10])
-	}
-
-	return result
-}
-
-// gets all signature hashes of given ABI
-func (data *AbiStorage) GetTopics() []string {
-	result := make([]string, 0)
-
-	for _, event := range data.Abi.Events {
-		topic := ToSHA3(event.Sig)
-		result = append(result, topic)
-	}
-
-	return result
-}
-
-// gets all signatures of given ABI
-func (data *AbiStorage) GetSignatures() []string {
-	result := make([]string, 0)
-
-	for _, event := range data.Abi.Events {
-		result = append(result, event.Sig)
-	}
-
-	for _, method := range data.Abi.Methods {
-		result = append(result, method.Sig)
-	}
-
-	return result
-}
-
-func (data *AbiStorage) ValidateBytecodes() *bool {
-	if data.Bytecode == nil {
-		return nil
-	}
-	sigs := make([]string, 0)
-	sigs = append(sigs, data.GetSigHashes()...)
-	sigs = append(sigs, data.GetTopics()...)
-	valid := DetectBytecodes(*data.Bytecode, sigs)
-	return &valid
 }
