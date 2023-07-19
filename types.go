392a10036b07748a3e745843c0cfaf58985a74a2
w2496 <139476228+w2496@users.noreply.github.com>
2023-07-19 03:01:51 +0200

    Update types.go

--- types.go
+++ types.go
@@ -3,6 +3,8 @@ package decoder
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -10,14 +12,42 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+type Params map[string]interface{}
+
+func (m *Params) MarshalJSON() ([]byte, error) {
+	regex := regexp.MustCompile(`^"0x[0-9a-fA-F]{40}"$`)
+	result := "{"
+	var parts []string
+
+	for k, v := range *m {
+		part, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+
+		if regex.MatchString(string(part)) {
+			addr := common.HexToAddress(strings.ReplaceAll(string(part), "\"", ""))
+			parts = append(parts, fmt.Sprintf(`"%s":"%s"`, k, addr.Hex()))
+		} else {
+			parts = append(parts, fmt.Sprintf(`"%s":%s`, k, string(part)))
+		}
+	}
+
+	result += strings.Join(parts, ",")
+	result += "}"
+
+	return []byte(result), nil
+}
+
 // DecodedLog is a struct for holding decoded Ethereum logs.
 type DecodedLog struct {
-	Contract        string                 `json:"contract"`        // Contract address of the decoded log.
-	Topic           string                 `json:"topic"`           // Event topic hash of the decoded log.
-	Signature       string                 `json:"signature"`       // Event signature of the decoded log.
-	Params          map[string]interface{} `json:"params"`          // Parameters of the decoded log.
-	TransactionHash string                 `json:"transactionHash"` // Transaction hash of the decoded log.
-	LogIndex        uint                   `json:"logIndex"`        // Index of the decoded log
+	Contract        string `json:"contract"`        // Contract address of the decoded log.
+	Topic           string `json:"topic"`           // Event topic hash of the decoded log.
+	Signature       string `json:"signature"`       // Event signature of the decoded log.
+	Params          Params `json:"params"`          // Parameters of the decoded log.
+	TransactionHash string `json:"transactionHash"` // Transaction hash of the decoded log.
+	LogIndex        uint   `json:"logIndex"`        // Index of the decoded log
+	BlockNumber     uint64 `json:"blockNumber"`     // blockNumber of given decoded log
 }
 
 // ToJSONBytes returns the JSON-encoded byte array of the DecodedLog object.
@@ -38,6 +68,7 @@ func (data *DecodedLog) GetParamsJSON() string {
 	}
 
 	b, _ := json.Marshal(data.Params)
+
 	return string(b)
 }
 
@@ -61,11 +92,11 @@ func (data *DecodedLog) GetSigHash() string {
 
 // DecodedMethod is a struct for holding decoded Ethereum methods.
 type DecodedMethod struct {
-	TransactionHash string                 `json:"transactionHash"` // Transaction hash of the decoded method.
-	Contract        string                 `json:"contract"`        // Contract address of the decoded method.
-	SigHash         string                 `json:"sigHash"`         // Function selector hash of the decoded method.
-	Signature       string                 `json:"signature"`       // Function signature of the decoded method.
-	Params          map[string]interface{} `json:"params"`          // Parameters of the decoded method.
+	TransactionHash string `json:"transactionHash"` // Transaction hash of the decoded method.
+	Contract        string `json:"contract"`        // Contract address of the decoded method.
+	SigHash         string `json:"sigHash"`         // Function selector hash of the decoded method.
+	Signature       string `json:"signature"`       // Function signature of the decoded method.
+	Params          Params `json:"params"`          // Parameters of the decoded method.
 }
 
 // ToJSONBytes returns the JSON-encoded byte array of the DecodedMethod object.
